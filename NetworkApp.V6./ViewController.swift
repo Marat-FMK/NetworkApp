15bd886071e29101a5dc54c2c44092349af9d29b
MaratXcode <kosjachok8@yandex.ru>
2024-07-09 15:58:02 +0300

    Initial Commit

---
+++ NetworkApp.V6./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkApp.V6.
+//
+//  Created by Marat Fakhrizhanov on 09.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
